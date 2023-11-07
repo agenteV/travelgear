c28f466e46d4171d3d5c7713e717698253e05ea1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 11:37:06 -0300

    TRAVELGEAR - AC funcionando perfeitamente

--- travelgear.js
+++ travelgear.js
@@ -1,86 +1,72 @@
-const mongoose = require("mongoose")
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/travelgear',
-{   useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb://127.0.0.1:27017/travelgear', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
-
 
 const UsuarioSchema = new mongoose.Schema({
-    email : {type : String, required : true},
-    senha : {type : String}
-});
+    email: { type: String, required: true },
+    senha: { type: String }
+}, { timestamps: true }); 
+
 const ProdutoviagemSchema = new mongoose.Schema({
-    id_produtoviagem : {type : String, required : true},
-    descricao : {type : String},
-    fornecedor : {type : String},
-    dataFabricacao : {type : Date},
-    quantidadeEstoque : {type: Number}
-})
+    id_produtoviagem: { type: String, required: true },
+    descricao: { type: String },
+    fornecedor: { type: String },
+    dataFabricacao: { type: Date },
+    quantidadeEstoque: { type: Number }
+}, { timestamps: true }); 
 
 const Usuario = mongoose.model("Usuario", UsuarioSchema);
 const Produtoviagem = mongoose.model("Produtoviagem", ProdutoviagemSchema);
 
-app.post("/cadastrousuario" , async(req , res)=>{
-    const email = req.body.email;
-    const senha = req.body.senha
+app.post("/cadastrousuario", async (req, res) => {
+    const { email, senha } = req.body;
 
     const usuario = new Usuario({
-        email : email,
-        senha : senha  
-        
+        email: email,
+        senha: senha,
     });
-        try{
-            const newUsuario = await usuario.save();
-            res.json({error : null, msg : "Cadastro ok ", UsuarioId : newUsuario._id});
-        }catch (error) {}
-        
 
-
+    try {
+        const newUsuario = await usuario.save();
+        res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
-// 2 model
-app.post("/cadastroproduto", async(req , res)=>{
-    const id_produtoviagem = req.body.id_produtoviagem;
-    const descricao = req.body.descricao;
-    const fornecedor = req.body.fornecedor;
-    const dataFabricacao = req.body.dataFabricacao;
-    const quantidadeEstoque = req.body.quantidadeEstoque
+app.post("/cadastroproduto", async (req, res) => {
+    const { id_produtoviagem, descricao, fornecedor, dataFabricacao, quantidadeEstoque } = req.body;
 
     const produtoviagem = new Produtoviagem({
-        id_produtoviagem : id_produtoviagem,
-        descricao : descricao,
-        fornecedor : fornecedor,
-        dataFabricacao : dataFabricacao,
-        quantidadeEstoque : quantidadeEstoque
+        id_produtoviagem: id_produtoviagem,
+        descricao: descricao,
+        fornecedor: fornecedor,
+        dataFabricacao: dataFabricacao,
+        quantidadeEstoque: quantidadeEstoque
     });
-    try{
+
+    try {
         const newProduto = await produtoviagem.save();
-        res.json({error : null, msg : "Cadastro ok ", ProdutoId : newProduto._id});
-    }catch (error) {}
+        res.json({ error: null, msg: "Cadastro ok", ProdutoId: newProduto._id });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
-app.get("/cadastrousuario", async(req, res)=>{
-    res.sendFile(__dirname +"/cadastrousuario.html");
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/cadastroproduto", async(req, res)=>{
-    res.sendFile(__dirname +"/cadastroproduto");
-})
-
-app.get("/", async(req, res)=>{
-    res.sendFile(__dirname +"/index.html");
-})
-
-//configurando a porta
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
-})
-    
-
+});
